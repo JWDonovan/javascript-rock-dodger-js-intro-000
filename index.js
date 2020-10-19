5a46b5fbd248127406274bbb694a2bec4d51738d
James Donovan <jameswilliamdonovan@gmail.com>
2020-10-19 18:24:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -128,6 +128,7 @@ function createRock(x) {
   }
 
   // We should kick off the animation of the rock around here.
+  window.requestAnimationFrame();
 
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision.
