4abb805e293cd27ad88484f214f0410fd6425a40
James Donovan <jameswilliamdonovan@gmail.com>
2020-10-19 18:13:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function checkCollision(rock) {
        (rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerLeftEdge) ||
        (rockLeftEdge > dodgerLeftEdge && rockRightEdge < dodgerRightEdge) ||
        (rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge) ||
-       (top > GAME_HEIGHT - 40)) {
+       (top < GAME_HEIGHT - 40)) {
       return true;
     }
 
