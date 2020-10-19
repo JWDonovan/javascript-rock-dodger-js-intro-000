d0b044f8f498c0815d7c110484e0631739bb6f68
James Donovan <jameswilliamdonovan@gmail.com>
2020-10-19 17:32:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -143,6 +143,7 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+
 }
 
 function moveDodgerRight() {
