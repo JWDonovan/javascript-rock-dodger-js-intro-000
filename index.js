479fa6285361800c1c377fb4edb8a9a740df376e
James Donovan <jameswilliamdonovan@gmail.com>
2020-10-19 17:51:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ function moveDodgerRight() {
     var leftNumbers = DODGER.style.left.replace('px', '');
     var left = parseInt(leftNumbers, 10);
 
-    if (left < GAME_WIDTH - DODGER.style.width) {
+    if (left < GAME_WIDTH - 40) {
       DODGER.style.left = `${left + 4}px`;
     }
   }
