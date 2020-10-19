4e098a637cc81061461701dc6d76bfb544640ae3
James Donovan <jameswilliamdonovan@gmail.com>
2020-10-19 17:39:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ function moveDodgerLeft() {
     var leftNumbers = DODGER.style.left.replace('px', '');
     var left = parseInt(leftNumbers, 10);
     DODGER.style.left = `${left - 4}px`;
-    window.requestAnimationFrame(step)
+    window.requestAnimationFrame(step);
   }
 }
 
